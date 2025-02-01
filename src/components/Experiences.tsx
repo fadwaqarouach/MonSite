eb37b12f127f8111ea556794e98c094c4780b4f6
fadwaqarouach <fqarouach@gmail.com>
2025-02-01 13:47:37 +0100

    first commit

---
+++ src/components/Experiences.tsx
@@ -0,0 +1,110 @@
+import Title from "./Title"
+
+import imgCSS from "../assets/techno/css.png";
+import imgJS from "../assets/techno/js.png";
+import imgREACT from "../assets/techno/react.png";
+import imgHTML from "../assets/techno/html.png";
+import imgNEXT from "../assets/techno/next-js.webp";
+import imgNODE from "../assets/techno/node-js.png";
+import imgTYPE from "../assets/techno/typescript.svg";
+import imgTAILWIND from "../assets/techno/tailwind.png";
+import imgPRISMA from "../assets/techno/prisma.webp";
+
+/*import google from "../assets/companies/google.png";
+import meta from "../assets/companies/meta.webp";
+import amazon from "../assets/companies/amazon.png";*/
+
+
+
+const skills = [
+    { id: 1, name: "HTML", image: imgHTML },
+    { id: 2, name: "CSS", image: imgCSS },
+    { id: 3, name: "JavaScript", image: imgJS },
+    { id: 4, name: "React", image: imgREACT },
+    { id: 5, name: "Node.js", image: imgNODE },
+    { id: 6, name: "Tailwind CSS", image: imgTAILWIND },
+    { id: 7, name: "TypeScript", image: imgTYPE },
+    { id: 8, name: "Next.js", image: imgNEXT },
+    { id: 9, name: "Prisma", image: imgPRISMA },
+];
+
+
+const experiences = [
+    {
+        id: 1,
+        role: "Administrateur Systèmes et Réseaux",
+        company: " KH INDUSTRIE",
+        period: "Mars 2020- juillet 2023 ",
+        description: [
+            "Supervision et maintenance des infrastructures IT.",
+            "Gestion des incidents utilisateurs, dépannage et suivi des tickets.",
+            "Déploiement de solutions techniques : serveurs, routeurs, sauvegardes..",
+            "Rédaction de rapports techniques et documentation client..",
+        ],  
+    },
+    {
+        id: 2,
+        role: "Testeur de Logiciel, Stagiaire",
+        company: "Auto Hall",
+        period: "Septembre 2019- Février 2020",
+        description: [
+            "Vérification des corrections apportées après chaque phase de test.",
+            "Identification des exigences du logiciel.",
+            "Analyse des résultats des tests et contrôle de la qualité des données.",
+            "Rédaction de rapports détaillés destinés aux développeurs afin de faciliter les corrections.",
+            
+        ],
+        //image: meta,
+    },
+];
+
+
+
+const Experiences = () => {
+    return (
+        <div id="Experiences">
+            <Title title="Expériences" />
+            <div className="flex  flex-col-reverse md:flex-row justify-center items-center">
+                <div className="flex flex-wrap gap-4  justify-center items-center md:w-1/3 mt-4 md:mt-0">
+                    {skills.map((skill) => (
+                        <div key={skill.id} className=" flex justify-center items-center flex-col">
+                            <div className="w-24 h-24 p-2 rounded-full border-2 border-accent">
+                                <img src={skill.image} alt={skill.name}
+                                    className="object-cover rounded-full h-full w-full"
+                                />
+                            </div>
+                            <span className="mt-2 text-sm">{skill.name}</span>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="md:ml-4 flex flex-col space-y-4">
+                    {experiences.map((expericence) => (
+                        <div
+                            key={expericence.id}
+                            className="flex flex-col bg-base-200 p-5 rounded-xl shadow-lg"
+                        >
+                            <div className="flex items-center">
+                                <div className="ml-4">
+                                    <h1 className="text-xl text-info font-bold">
+                                        {expericence.role} , {expericence.company}
+                                    </h1>
+                                    <span className="text-sm">{expericence.period}</span>
+                                </div>
+                            </div>
+                            <ul className="list-disc ml-16 mt-2">
+                                {expericence.description.map((desc, index) => (
+                                    <li key={index}>
+                                        {desc}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experiences
